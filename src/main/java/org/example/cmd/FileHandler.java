9e5dccc848dea07dc746285f58cead4fa153fd2a
Zhang Y.D. <cnfighteraimi@gmail.com>
2025-04-30 21:36:17 -0400

    Update: Sample saveTasks in FileHandler

--- src/main/java/org/example/cmd/FileHandler.java
+++ src/main/java/org/example/cmd/FileHandler.java
@@ -1,12 +1,43 @@
 package org.example.cmd;
 
+import org.example.task.RegularTask;
 import org.example.task.Task;
+import org.example.TaskManager;
+import org.example.task.UrgentTask;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class FileHandler {
+    static File file = new File("src/main/resources/tasks.csv");
+
     public static void saveTasks(String filePath) {
-        //Todo
+        for (Task task : TaskManager.getTasks()) {
+            if (task instanceof RegularTask regularTask) {
+                try (FileWriter fileWriter = new FileWriter(file)) {
+                    fileWriter.write("regular" +
+                            "," + regularTask.getTitle() +
+                            "," + regularTask.getCreatedDate() +
+                            "," + regularTask.isCompleted() +
+                            "," + regularTask.getTimePassed() + "\n");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (task instanceof UrgentTask urgentTask) {
+                try (FileWriter fileWriter = new FileWriter(file)) {
+                    fileWriter.write("urgent" +
+                            "," + urgentTask.getTitle() +
+                            "," + urgentTask.getCreatedDate() +
+                            "," + urgentTask.isCompleted() +
+                            "," + urgentTask.getDueDate() +
+                            "," + urgentTask.getPriority() + "\n");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
     public static void loadTasks(String filePath) {
         //Todo
